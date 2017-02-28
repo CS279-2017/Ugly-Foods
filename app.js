f6b8b2ead5300b5a56f564065d3e0de786abddeb
Alec Lindner <alec.r.lindner@vanderbilt.edu>
2017-02-27 20:52:17 -0600

    order placing

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
-
+const orderController = require('./controllers/order')
 /**
  * API keys and Passport configuration.
  */
@@ -116,6 +116,7 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
  * Primary app routes.
  */
 app.get('/', homeController.index);
+app.get('/order', passportConfig.isAuthenticated, orderController.index)
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
